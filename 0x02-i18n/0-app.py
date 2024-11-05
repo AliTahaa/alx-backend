98835880437e0719aeb497d98a9abc1a152b2943
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-11-05 21:35:50 +0200

    try

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,10 +7,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-  """Default route"""
-  return render_template("0-index.html")
+	"""Default route"""
+	return render_template("0-index.html")
+
 
 if __name__ == "__main__":
-  app.run(debug=True)
+	app.run(debug=True)
